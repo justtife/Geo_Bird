15e68d904d927951bf75fb9dc7e6fc17097bb695
Justtife <farinubolu@gmail.com>
2024-03-20 11:27:01 +0000

    Update made to the login component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,30 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import {
+  GoogleLoginProvider,
+} from '@abacritt/angularx-social-login';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(),
+  {
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            "32156017357-kh2petjdic710rdbjlp0jnql7tikq8m7.apps.googleusercontent.com"
+          )
+        },
+      ],
+      onError: (err) => {
+        console.log("This is the error")
+        console.error(err);
+      }
+    } as SocialAuthServiceConfig,
+  }
+  ]
 };
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+import { SocialAuthService, GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, GoogleSigninButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
+  constructor(private authService: SocialAuthService) { }
+  ngOnInit(): void {
+    this.authService.authState.subscribe((user) => {
+      console.log(user)
+    });
+  }
 }
